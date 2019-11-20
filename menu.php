b6e3d6ceea3969aa821aa814bc0da1afd25ede32
Evabas <51789607+Evabas@users.noreply.github.com>
2019-11-20 10:52:31 +0100

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,21 @@
+<nav id="menu" class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+            <a class="navbar-brand" href="index.php">Jean Forteroche</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                <li class="nav-item">
+                        <a class="nav-link" href="chapitres.php">Chapitres</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="auteur.php">Auteur</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="espaceMembres.php">Connexion</a>
+                    </li>
+                </ul>
+    
+            </div>
+        </nav>
+          
