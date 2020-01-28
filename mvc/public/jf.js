b577aace4e5435ea0aea20e6f6b6f48b394e6cb2
Evabas <51789607+Evabas@users.noreply.github.com>
2020-01-28 16:00:06 +0100

    Delete jf.js

--- mvc/public/jf.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-
-    var connect = document.getElementById("connexion");
-    if (isset($_SESSION[''])) {
-        document.connect.style.display = "none";
-    } else {
-        document.connect.style.display = "block";
-    };
-});
